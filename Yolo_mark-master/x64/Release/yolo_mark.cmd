8f65e7e89811198f7e5cc553ce126a72c4f85a64
dabbyp <dabbyp@naver.com>
2020-06-10 19:18:32 +0900

    project source upload
    Former-commit-id: e23c92648e6d88513da91ba7a221f3b8f338fe8d

---
+++ Yolo_mark-master/x64/Release/yolo_mark.cmd
@@ -0,0 +1,10 @@
+echo     Example how to start marking bouded boxes for training set Yolo v2
+
+@rem 600 x 400
+yolo_mark.exe data2/img data2/train.txt data2/obj.names
+
+@rem yolo_mark.exe data1/img data1/train.txt data1/obj.names
+@rem yolo_mark.exe data/img data/train.txt data/obj.names
+
+
+pause
